ea2f43771dd45be904259c47688adb32f8c0f69c
mf5876 <48483179+mf5876@users.noreply.github.com>
2020-04-14 18:32:51 -0400

    Add files via upload

--- Project Files/src/App.js
+++ Project Files/src/App.js
@@ -25,7 +25,6 @@ function App(){
         <div className = "App">
         <DenseAppBar></DenseAppBar>
         
-
              <Switch>  
           
                 <Route path = "/" exact component = {HomePage}/> 
