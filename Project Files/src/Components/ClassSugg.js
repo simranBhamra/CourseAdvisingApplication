9bb8814515562c959676a7846e898f42f452f674
dherrington102 <44878948+dherrington102@users.noreply.github.com>
2020-04-15 14:41:42 -0400

    updated comment suggestion box

--- Project Files/src/Components/ClassSugg.js
+++ Project Files/src/Components/ClassSugg.js
@@ -10,9 +10,13 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import { spacing } from '@material-ui/system';
+import CustomizedTables from './Table.js';
 
+var app = window.require('electron').remote;
+const fs = app.require('fs');
 
-
+const courseDataPath = './src/data/courseTimes.json';
+var courseData = require('../data/courseTimes.json')
 
 
 const useStyles = makeStyles(theme => ({
@@ -24,7 +28,8 @@ const useStyles = makeStyles(theme => ({
     padding: theme.spacing(2),
  
     width: 450,
-    maxHeight: 10000
+    maxHeight: 700,
+    
   },
   image: {
     width: 128,
@@ -65,6 +70,8 @@ const useStyles = makeStyles(theme => ({
 export default function CommentBox() {
    const classes = useStyles();
 
+   
+
   return (
     <div className={classes.root}>
       <Paper className={classes.paper} >
@@ -79,8 +86,8 @@ export default function CommentBox() {
    
         <Grid container className="qNumGrid" xs={12}>
             
-            <Grid item xs={12} className={classes.textField}>
-            <TextField
+      <Grid item xs={12} className={classes.textField}>
+        <TextField
           id="filled-full-width"
           label="Search"
           style={{  top_margin: 2 }}
@@ -91,43 +98,16 @@ export default function CommentBox() {
           }}
           variant="outlined"
         />
-        <Button variant="contained" size="large" className={classes.margin} style ={{ color: "white", marginTop: '20px',marginBottom: '20px' ,backgroundColor:"#36BF26", minWidth: "100%"}}>
-        CS-356      
-        <br></br>
-        Software Engineering  
-        </Button>
+      <Paper style={{maxHeight: 550, overflow: 'auto', border: 'none'}}>
+        {courseData.data.map(element=>( 
+          <Button variant="contained" size="large"  className={classes.margin} style ={{ color: "white", marginTop: '20px',marginBottom: '20px' ,backgroundColor:"#36BF26", minWidth: "100%"}}>
+          {element.title}      
+          <br></br>
+          {element.course}  
+          </Button>
+        ))}
 
-        <Button variant="contained" size="large"  className={classes.margin} style ={{ color: "white",marginBottom: '20px',backgroundColor:"#36BF26", minWidth: "100%"}}>
-        CS-360  
-        <br></br>
-        Database Analysis and Design
-        </Button>
-
-        <Button variant="contained" size="large"  className={classes.margin} style ={{ color: "white",marginBottom: '20px',backgroundColor:"#FFA70E", minWidth: "100%"}}>
-        PS-265
-        <br></br>
-        Psychobiology
-        </Button>
-
-        <Button variant="contained" size="large"  className={classes.margin} style ={{ color: "white",marginBottom: '20px',backgroundColor:"#742AE4", minWidth: "100%"}}>
-        CJ-160
-        <br></br>
-        Criminal Law
-        </Button>
-        
-
-        <Button variant="contained" size="large"  className={classes.margin} style ={{ color: "white",marginBottom: '20px',backgroundColor:"#742AE4", minWidth: "100%"}}>
-        CJ-260
-        <br></br>
-        Law Enforcement
-        </Button>
-
-        <Button variant="contained" size="large"  className={classes.margin} style ={{ color: "white",marginBottom: '33px',backgroundColor:"#03CFFF", minWidth: "100%"}}>
-        HI-204
-        <br></br>
-        History of Latin America
-        </Button>
-
+      </Paper> 
 
             </Grid>
         </Grid>
