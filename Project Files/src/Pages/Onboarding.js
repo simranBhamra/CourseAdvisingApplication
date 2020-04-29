08497d8faf9328b11ccf175c32a829f712f812d4
mf5876 <48483179+mf5876@users.noreply.github.com>
2020-04-29 18:23:10 -0400

    Add files via upload

--- Project Files/src/Pages/Onboarding.js
+++ Project Files/src/Pages/Onboarding.js
@@ -7,6 +7,11 @@ import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import data from '../data/courses.json'
+import SelectClasses from '../Components/SelectClasses'
+
 const useStyles = makeStyles(theme => ({
 
     spinner: {
@@ -57,6 +62,12 @@ const useStyles = makeStyles(theme => ({
     },
   };
 
+var app = window.require('electron').remote;
+const fs = app.require('fs');
+
+const userDataPath = './src/data/basicUserInfo.json';
+let userData = require('../data/basicUserInfo.json')
+
 const years = [
     'Freshman',
     'Sophomore',
@@ -68,19 +79,58 @@ const majors = [
     'Mathematics',
 ];
 
-function saveButtonClicked(){
-    console.log("Fuck");
-}
+let courses = data.courses.map(
+    function (course) {
+        return (course.course) + ": " + (course.title)
+    }
+)
+
+
+
 
 
 function Profile(){
 
-    const classes = useStyles();
-    const [classYear, setClassYear] = React.useState([]);
+    const [dialogOpen, setDialogOpen] = React.useState(false);
+
+    const handleDialogOpen = () => {
+        setDialogOpen(true);
+      };
     
-    const handleChange = event => {
+      const handleDialogClose = () => {
+        setDialogOpen(false);
+      };
+
+    const classes = useStyles();
+    const [name, setName] = React.useState("")
+    const [classYear, setClassYear] = React.useState();
+    const [major, setMajor] = React.useState();
+    const [minor, setMinor] = React.useState();
+    const [classArray, setClassArray] = React.useState([])
+    
+    const classChange = event => {
         setClassYear(event.target.value);
     };
+
+    function onSave(){
+        userData[0].name = name
+        userData[0].class = classYear
+        userData[0].primaryMajor = major
+        userData[0].minor = minor
+        userData[0].selectedCourses = classArray
+
+        console.log("sfddfs")
+        console.log(userData[0])
+        console.log("fsdsdf")
+
+        fs.writeFile(userDataPath, JSON.stringify(userData[0]), function writeJSON(err) {
+            if (err) return console.log(err);
+            console.log(JSON.stringify(userData[0]));
+            console.log('writing to ' + userDataPath);
+          });
+    
+          console.log('saved user info')
+    }
 
     return (
         <div>
@@ -89,7 +139,9 @@ function Profile(){
                 <h1>Onboarding Page</h1>
                 <div>
                     <b  className={classes.left}>Name:</b>
-                    <Input className={classes.right}/>
+                    <Input className={classes.right} onChange={(e)=>{
+                        setName(e.target.value)
+                    }}/>
                 </div>
                 <div>
                     <b className={classes.left}>Class Level:</b>
@@ -99,7 +151,6 @@ function Profile(){
                         value={classYear}
                         placeholder="Class"
                         style={{alignText:'center'}}
-                        onChange={handleChange}
                         input={<Input />}
                     >
                         {years.map(year => (
@@ -116,6 +167,10 @@ function Profile(){
                         options={majors}
                         id="debug"
                         debug
+                        value={major}
+                        onChange={(event, value)=>{
+                            setMajor(value)
+                        }}
                         style = {{width: 300}}
                         renderInput={(params) => <TextField {...params} label="Major"/>}
                     />
@@ -128,6 +183,9 @@ function Profile(){
                         id="debug"
                         debug
                         style = {{width: 300}}
+                        onChange={(event, value)=>{
+                            setMinor(value)
+                        }}
                         renderInput={(params) => <TextField {...params} label="Minor"/>}
                     />
                 </div>
@@ -138,10 +196,14 @@ function Profile(){
                             variant="contained"
                             color="secondary"
                             className={classes.button}
-                            onClick={saveButtonClicked()}
+                            onClick={handleDialogOpen}
                         >
                             View/Add
                         </Button>
+
+                        <Dialog open={dialogOpen} onClose={handleDialogClose}>
+                            <SelectClasses setClassArray={setClassArray} handleDialogClose={setDialogOpen}></SelectClasses>
+                        </Dialog>
                     </div>
                 </div>
                 <div>
@@ -150,8 +212,20 @@ function Profile(){
                     color="secondary"
                     className={classes.button}
                     style={{marginTop:150, width:300}}
+                    onClick={onSave}
                 >
                     Save
+                </Button>
+
+                <Button style={{width: 54, height: 54, backgroundColor: 'red'}}onClick={()=>{
+                    console.log(classYear)
+                    console.log(major)
+                    console.log(minor)
+                    console.log(data.courses[0].course)
+                    console.log(courses)
+                    console.log(data)
+                    console.log(classArray)
+                }}>
                 </Button>
                 </div>
             </form>
